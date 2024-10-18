285b9e487aef8efdc34f00494644e1f03b95c172
Vladut Gidea <vladutgidea.ro@gmail.com>
2024-10-18 18:57:32 +0300

    added department creation&editor pages

--- EmployeeManagementLibrary/DB/DepartmentData.cs
+++ EmployeeManagementLibrary/DB/DepartmentData.cs
@@ -12,5 +12,40 @@
         {
             return await _context.Departments.ToListAsync();
         }
+
+        public async Task AddDepartment(DepartmentModel departmentModel)
+        {
+            if (departmentModel != null)
+            {
+                _context.Departments.Add(departmentModel);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task RemoveDepartment(int departmentId)
+        {
+            var department = await _context.Departments.FindAsync(departmentId);
+            if (department != null)
+            {
+                _context.Departments.Remove(department);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task<DepartmentModel> FindDepartment(int departmentId)
+        {
+            return await _context.Departments.FindAsync(departmentId);
+        }
+        public async Task UpdateDepartment(DepartmentModel departmentModel)
+        {
+            if (departmentModel != null)
+            {
+                var existingDepartment = await _context.Departments.FindAsync(departmentModel.DepartmentId);
+                if (existingDepartment != null)
+                {
+                    existingDepartment.DepartmentName = departmentModel.DepartmentName;
+                }
+                else { throw new InvalidOperationException("Department does not exist"); }
+            }
+        }
     }
 }
--- EmployeeManagementLibrary/DB/IDepartmentData.cs
+++ EmployeeManagementLibrary/DB/IDepartmentData.cs
@@ -4,5 +4,10 @@ namespace EmployeeManagementLibrary.DB
     public interface IDepartmentData
     {
         Task<List<DepartmentModel>> Departments();
+
+        Task AddDepartment(DepartmentModel departmentModel);
+        Task RemoveDepartment(int departmentId);
+        Task<DepartmentModel> FindDepartment(int departmentId);
+        Task UpdateDepartment(DepartmentModel departmentModel);
     }
 }
